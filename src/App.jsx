647b503eddc3039774d5b23684780d2600662b13
Wren Purcell <purcell856@gmail.com>
2022-12-07 21:09:35 -0500

    took out commented out chunk

--- src/App.jsx
+++ src/App.jsx
@@ -31,45 +31,7 @@ function Layout(props) {
   return <div className="layout">{props.children}</div>;
 }
 
-// function Login() {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [error, setError] = useState(false);
 
-//   const handleLogin = (e) => {
-//     e.preventDefault();
-
-//     if (username == "user123" && password == "password123") {
-//       console.log("logged in");
-//       setError(false);
-//     } else {
-//       setError(true);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {error ? (
-//         <div>That was the wrong username and/or password</div>
-//       ) : (
-//         <div>Welcome to the app</div>
-//       )}
-//       <form onSubmit={(e) => handleLogin(e)}>
-//         <input
-//           onChange={(e) => setUsername(e.target.value)}
-//           type="text"
-//           name="username"
-//         />
-//         <input
-//           onChange={(e) => setPassword(e.target.value)}
-//           type="password"
-//           name="password"
-//         />
-//         <input type="submit" value="Login" />
-//       </form>
-//     </div>
-//   );
-// }
 
 function App() {
   const [isUserLoggedIn, setUserLoggedIn] = useState(false);
@@ -96,7 +58,7 @@ function App() {
          : 
          <>
          {/* signup page
-          
+
          
          */}
          </>
